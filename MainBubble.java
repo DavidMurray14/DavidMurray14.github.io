2823a24373353de16cf9669b53a12605f6da2641
DavidMurray14 <emmitt.murray@malad.us>
2025-10-02 11:34:48 -0600

    Add files via upload

--- MainBubble.java
+++ MainBubble.java
@@ -1,26 +1,27 @@
 // I am Emmitt. This program is supposed to bubble sort an array.
 public class MainBubble {
-
+//Bubble sort code.
     public static void main(String[] args) {
-        int[] array = {5, 3, 8, 4, 2};
+        int[] array = {5, 3, 8, 4, 2}; //Numbers to be sorted.
+        //Main program.
         boolean isSorted = false;
         while (!isSorted) {
             isSorted = true;
             for (int i = 0; i < array.length - 1; i++) {
                 if (array[i] > array[i + 1]) {
-                    swap(array, i, i + 1);
+                    swapTwoNumbers(array, i, i + 1); //Using the swap method.
                     isSorted = false;
                 }
             }
         }
         for (int num : array) {
-            System.out.print(num + " ");
+            System.out.print(num + " "); //Printing the final result after sorting. 
         }
     }
-
-    private static void swap(int[] arr, int i, int j) {
+//Code for swapping two numbers.
+    private static void swapTwoNumbers(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = temp;
+        arr[j] = temp; 
     }
 }
